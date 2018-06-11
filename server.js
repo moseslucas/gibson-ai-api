c024b448f30ee8b223c852414a9c648bde26e884
moseslucas <mo@moseslucas.com>
2018-06-11 17:21:05 +0800

    Adds learn endpoint

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const T = new Twit({
   access_token_secret: 'srWb6B4NGFAIvc0L8i1Yc4rM6TW74IYjeA6FS1KV63ZPX'
 })
 const BrainJSClassifier = require('natural-brain')
+let classifier
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -35,34 +36,25 @@ app.post('/twit/search', (req, res) => {
 })
 
 app.post('/twit/tweets', (req, res) => {
-  T.get('statuses/user_timeline', { screen_name: req.body.q, count: 50}, function(err, data, response) {
+  T.get('statuses/user_timeline', { screen_name: req.body.q, count: 10}, function(err, data, response) {
     res.send(data)
   })
 })
 
-app.post('/twit/guess', (req, res) => {
-  const classifier = new BrainJSClassifier()
+app.post('/twit/learn', (req, res) => {
+  classifier = new BrainJSClassifier()
   const twits = req.body.twits
-  const guess = req.body.guess
-  // twits.map( twit => {classifier.addDocument(twit.text, twit.username)})
-  // classifier.train()
-  // console.log('train: ', classifier.train())
-  // console.log('guessed: ', classifier.classify(guess))
-  // res.send(classifier.classify(guess))
-  // console.log(classifier.classify('tae'))
-  // twits.map( twit => {
-  //   classifier.addDocument(twit.text, twit.username)
-  // })
   for (i = 0; i < twits.length; i++) {
     console.log('Adding to dataset')
     classifier.addDocument(twits[i].text, twits[i].username)
   }
-  // classifier.addDocument(twits[0].text, twits[0].username)
-  // classifier.addDocument(twits[59].text, twits[59].username)
-
-  console.log('training')
   classifier.train()
+  console.log('learned')
+  res.send('OK')
+})
 
+app.post('/twit/guess', (req, res) => {
+  const guess = req.body.guess
   res.send(classifier.classify(guess))
 })
 
